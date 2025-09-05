4021813c21c8a27c59d62ad62ed4881b3780fe22
lvcasxm <lucasmaiaxgo@gmail.com>
2025-09-05 13:29:14 -0300

    exercicio 5 lista 1

--- aeds1/assignments/first-list/0105.c
+++ aeds1/assignments/first-list/0105.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 #include <locale.h>
+#include <math.h>
 
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
 
-    
+    double x = 0;
+    double y = 0;
 
-    
+    printf("X: ");
+    scanf("%d", &x);
+
+    if (x <= 1)
+    {
+        y = 1;
+    } else if (x <= 2)
+    {
+        y = 2;
+    } else if (x <= 3)
+    {
+        y = pow(x, 2);
+    } else
+    {
+        y = pow(x, 3);
+    }
+
+    printf("O valor de y é: %.2flf\n", y)
+
+    return 0;
 }
